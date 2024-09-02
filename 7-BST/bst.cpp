94448e849b520b0d2f2061445ed7a32f45efc2bf
Quacciosi <andreaquatrosi@gmail.com>
2024-09-02 14:02:38 +0200

    commit from laptop

--- 7-BST/bst.cpp
+++ 7-BST/bst.cpp
@@ -4,211 +4,205 @@ using namespace std;
 
 template <typename T>
 class Node {
+    
+    private:
+        T key;
+        Node<T>* left;
+        Node<T>* right;
 
-  private:
-    T key;
-    Node<T>* right;
-    Node<T>* left;
+    public:
+        Node() : key(T()), left(nullptr), right(nullptr) {}
+        Node(T key) : key(key), left(nullptr), right(nullptr) {}
 
-  public:
-    Node() : key(T()), right(nullptr), left(nullptr) {}
-    Node(T key) : key(key), right(nullptr), left(nullptr) {}
+        // Getter
+        T get_key() const { return key; }
+        Node<T>*& get_left() { return left; }
+        Node<T>*& get_right() { return right; }
 
-    // Getter
-    T get_key() const { return key; }
-    Node<T>*& get_right() { return right; } 
-    Node<T>*& get_left() { return left; } 
-
-    // Setter
-    void set_key(const T key) { this->key = key; }
-    void set_right(Node<T>* right) { this->right = right; }
-    void set_left(Node<T>* left) { this->left = left; }
+        // Setter
+        void set_key(const T key) { this->key = key; }
+        void set_left(Node<T>* left) { this->left = left; }
+        void set_right(Node<T>* right) { this->right = right; }
 };
 
 template <typename T>
-class BST {
+class BST { 
 
-  private:
-    Node<T>* root;
+    private:
+        Node<T>* root;
 
-    void add_node(Node<T>*& root, const T& key) {
-      
-      if(!root) { // setting the root
-        Node<T>* newNode = new Node<T>(key);
-        root = newNode;
+        void add_node(Node<T>*& root, const T& key) {
 
-        return;
-      }
+            if(!root) {
+                Node<T>* newNode = new Node<T>(key);
+                root = newNode;
 
-      // traversing the binary tree
-      if(key < root->get_key()) 
-        add_node(root->get_left(), key);
-      else if(key > root->get_key())
-        add_node(root->get_right(), key);
-    }
+                return;
+            }
 
-    void delete_node(Node<T>*& root, const T& key) {
-      if (!root) 
-          return;
+            if(key < root->get_key())
+                add_node(root->get_left(), key);
 
-      if (key < root->get_key())
-          delete_node(root->get_left(), key);
-      else if (key > root->get_key())
-          delete_node(root->get_right(), key);
-      else {
-          Node<T>* toDelete = nullptr;
+            else if(key > root->get_key())
+                add_node(root->get_right(), key);
+        }
 
-          if (!root->get_right() && !root->get_left()) {     // node has no children (leaf)
-              delete root;
-              root = nullptr;
-          }
+        void delete_node(Node<T>*& root, const T& key) {
 
-          else if (!root->get_left()) {                      // node has only right children
-              toDelete = root;
-              root = root->get_right();
+            if (!root) 
+                return;
 
-              delete toDelete;
-          }
+            if (key < root->get_key())
+                delete_node(root->get_left(), key);
+            else if (key > root->get_key())
+                delete_node(root->get_right(), key);
+            else {
+                Node<T>* toDelete = nullptr;
 
-          else if (!root->get_right()) {                    // node has only left children
-              toDelete = root;
-              root = root->get_left();
+                if (!root->get_right() && !root->get_left()) {     // node has no children (leaf)
+                    delete root;
+                    root = nullptr;
+                }
 
-              delete toDelete;
-          }
+                else if (!root->get_left()) {                      // node has only right children
+                    toDelete = root;
+                    root = root->get_right();
 
-          else {                                            // node has both left and right children
-              Node<T>* smallestRight = get_minNode(root->get_right());
+                    delete toDelete;
+                }
 
-              root->set_key(smallestRight->get_key());
-              delete_node(root->get_right(), smallestRight->get_key());
-          }
-      }
-    }
+                else if (!root->get_right()) {                    // node has only left children
+                    toDelete = root;
+                    root = root->get_left();
 
-    Node<T>* search_node(Node<T>*& root, const T& key) {
+                    delete toDelete;
+                }
 
-      if(!root)
-        return nullptr;
-      
-      if(key < root->get_key())
-        return search_node(root->get_left(), key);
-      else if(key > root->get_key())
-        return search_node(root->get_right(), key);
-      else
-        return root;
-    }
+                else {                                            // node has both left and right children
+                    Node<T>* smallestRight = get_minNode(root->get_right());
 
-    void inOrder(Node<T>* root) {
-      
-      if(!root)
-        return;
-      
-      inOrder(root->get_left());
-      cout << root->get_key() << " ";
-      inOrder(root->get_right());
-    }
+                    root->set_key(smallestRight->get_key());
+                    delete_node(root->get_right(), smallestRight->get_key());
+                }
+            }
+        }
 
-    void preOrder(Node<T>* root) {
+        Node<T>* get_maxNode(Node<T>* root) {
+            
+            Node<T>* current = root;
 
-      if(!root)
-        return;
+            /* since it's a BST, all (key > root) will be the right node*/
+            while(current->get_right())
+                current = current->get_right();
 
-      cout << root->get_key() << " ";
-      inOrder(root->get_left());
-      inOrder(root->get_right());
-    }
+            return current;
+        }
 
-    void postOrder(Node<T>* root) {
+        Node<T>* get_minNode(Node<T>* root) {
+            
+            Node<T>* current = root;
 
-      if(!root)
-        return;
+            /* since it's a BST, all (key < root) will be the left node*/
+            while(current->get_left())
+                current = current->get_left();
 
-      inOrder(root->get_left());
-      inOrder(root->get_right());
-      cout << root->get_key() << " ";
-    }
+            return current;
+        }
 
-    Node<T>* get_maxNode(Node<T>* root) {
+        void inOrder(Node<T>* root) const {
+            
+            if(!root)
+                return;
 
-      Node<T>* current = root;
+            inOrder(root->get_left());
+            cout << root->get_key() << " ";
+            inOrder(root->get_right());
+        }
 
-      while(current->get_right())
-        current = current->get_right();
+        void preOrder(Node<T>* root) const {
 
-      return current;
-    }
+            if(!root)
+                return;
 
-    Node<T>* get_minNode(Node<T>* root) {
+            cout << root->get_key() << " ";
+            preOrder(root->get_left());
+            preOrder(root->get_right());
+        }
 
-      Node<T>* current = root;
+        void postOrder(Node<T>* root) const {
 
-      while(current->get_left())
-        current = current->get_left();
+            if(!root)
+                return;
+            
+            postOrder(root->get_left());
+            postOrder(root->get_right());
+            cout << root->get_key() << " ";
+        }
 
-      return current;
-    }
+        Node<T>* search_node(Node<T>*& root, const T& key) {
 
-  public:
-    BST() : root(nullptr) {}
+            if(!root)
+                return nullptr;
 
-    void add_node(const T& key) {
-      add_node(root, key);
-    }
+            if(key < root->get_key())
+                return search_node(root->get_left(), key);
+            else if(key > root->get_key())
+                return search_node(root->get_right(), key);
+            else
+                return root;
+        }
 
-    void delete_node(const T& key) {
-      delete_node(root, key);
-    }
+    public:
+        BST() : root(nullptr) {}
 
-    Node<T>* search_node(const T& key) {
-      return search_node(root, key);
-    }
+        // Methods
+        void add_node(const T& key) {
+            add_node(root, key);
+        }
 
-    Node<T>* get_maxNode() {
-      get_maxNode(root);
-    }
+        void delete_node(const T& key) {
+            delete_node(root, key);
+        }
 
-    Node<T>* get_minNode() {
-      get_minNode(root);
-    }
+        void inOrder() const {
+            inOrder(root);
+        }
 
-    void inOrder() {
-      inOrder(root);
-    }
+        void preOrder() const {
+            preOrder(root);
+        }
 
-    void preOrder() {
-      preOrder(root);
-    }
-
-    void postOrder() {
-      postOrder(root);
-    }
+        void postOrder() const {
+            postOrder(root);
+        }
 };
 
 int main() {
 
-  BST<int> bst;
+    BST<int> bst;
 
-  constexpr size_t N = 8;
-  int nodes[N] = {10, 5, 20, 30, 4, 3, 15, 11};
+    constexpr size_t N = 8;
+    int nodes[N] = {1, 5, 2, 4, 10, 8, 3, 9};
 
-  for(size_t i = 0; i < N; i++)
-    bst.add_node(nodes[i]);
+    for(size_t i = 0; i < N; i++)
+        bst.add_node(nodes[i]);
+    
+    cout << "\nIn-Order visit:\n";
+    bst.inOrder();
 
-  Node<int>* node_tosearch = bst.search_node(11);
+    // bst.delete_node(1);
+    
+    cout << "\nPre-Order visit:\n";
+    bst.preOrder();
 
-  cout << node_tosearch->get_key();
+    cout << "\nPost-Order visit:\n";
+    bst.postOrder();
 
-  cout << "\nIn-Order visit:\n";
-  bst.inOrder();
+    cout << "\nAfter deleting: " << nodes[2] << " and " << nodes[1] << ":\n";
+    bst.delete_node(2);
+    bst.delete_node(5);
 
-  cout << "\nPre-Order visit:\n";
-  bst.preOrder();
+    bst.inOrder();
 
-  cout << "\nPost-Order visit:\n";
-  bst.postOrder();
-
-  bst.delete_node(10);
-
-  return 0;
+    return 0;
 }
