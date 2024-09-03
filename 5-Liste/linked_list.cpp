f2b6011359f1590d1db1b88e63edb45abfab7220
Quacciosi <andreaquatrosi@gmail.com>
2024-09-03 17:50:24 +0200

    commit from laptop

--- 5-Liste/linked_list.cpp
+++ 5-Liste/linked_list.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 template <typename T>
 class Node {
-
     private:
         T value;
         Node<T>* next;
@@ -15,15 +14,15 @@ class Node {
 
         // Getter
         T get_value() const { return value; }
-        Node<T>* get_next() const { return this->next; }
+        Node<T>* get_next() { return next; }
 
         // Setter
+        void set_value(const T value) { this->value = value; }
         void set_next(Node<T>* next) { this->next = next; }
 };
 
 template <typename T>
 class List {
-
     private:
         Node<T>* head;
 
@@ -44,139 +43,139 @@ class List {
         }
 
         // Operazioni
-        bool is_Empty() { return head == nullptr; }
-
-        void push_head(T value) {
-
-            Node<T>* newNode = new Node<T>(value); // creazione Nodo
-
-            newNode->set_next(head);    // newNode points to the current head
-            head = newNode;             // newNode is the new head
-        }
-
-        void push_tail(T value) {
-
-            Node<T>* newNode = new Node<T>(value);
-
-            if(this->is_Empty()) {
-                newNode->set_next(head);
-                head = newNode;
-            } else {
-                Node<T>* temp = head;
-
-                while(temp->get_next() != nullptr)      // going through the list 'till the last but one node
-                    temp = temp->get_next();
-
-                temp->set_next(newNode);                // setting the last but one node to points to the newNode
-            }
-        }
-
-        void push_sorted(T value) {
+        bool is_empty() { return head == nullptr; }
+    
+        void push_head(const T value) {
             
             Node<T>* newNode = new Node<T>(value);
 
-            if(this->is_Empty() || value < head->get_value()) {
+            if(this->is_empty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode->set_next(head);
+            head = newNode;
+        }
+
+        void push_tail(const T value) {
+
+            Node<T>* newNode = new Node<T>(value);
+
+            if(this->is_empty()) {
+                head = newNode;
+                return;
+            }
+
+            Node<T>* current = head;
+
+            while(current->get_next() != nullptr)
+                current = current->get_next();
+
+            current->set_next(newNode);
+        }
+
+        void push_sorted(const T value) {
+
+            Node<T>* newNode = new Node<T>(value);
+
+            if(this->is_empty() || value < head->get_value()) {
                 newNode->set_next(head);
                 head = newNode;
-            } else {
-                Node<T>* current = head;
-                Node<T>* prev = nullptr;
-
-                while(current != nullptr && current->get_value() < value) {
-                    prev = current;
-                    current = current->get_next();
-                }
-
-                newNode->set_next(current);
-                if(prev != nullptr)
-                    prev->set_next(newNode);
+                return;
             }
+
+            Node<T>* current = head;
+            Node<T>* prev = nullptr;
+
+            while(current != nullptr && value > current->get_value()) {
+                prev = current;
+                current = current->get_next();
+            }
+
+            newNode->set_next(current);
+            
+            if(prev != nullptr)
+                prev->set_next(newNode); 
         }
 
         T extract_head() {
-
-            if(this->is_Empty()) {
-                cout << "\nEmpty list!\n";
+            
+            if(this->is_empty())
                 exit(EXIT_FAILURE);
-            } else {
-                Node<T>* temp = head;
-                T value = head->get_value();
-                head = head->get_next();
 
-                delete temp;
-                
-                return value;
-            }
+            Node<T>* temp = head;
+
+            T value = head->get_value();
+            head = head->get_next();
+
+            delete temp;
+
+            return value;
         }
 
         T extract_tail() {
 
-            if(this->is_Empty()) {
-                cout << "\nEmpty list!\n";
+            if(this->is_empty())
                 exit(EXIT_FAILURE);
-            } else {
-                Node<T>* temp = head;
-                Node<T>* prev = nullptr;
 
-                while(temp->get_next() != nullptr) {
-                    prev = temp;
-                    temp = temp->get_next();
-                }
-
-                T value = temp->get_value();
-
-                if(prev != nullptr)
-                    prev->set_next(nullptr);
-                else
-                    head = nullptr; // the list has only one element
-
-                delete temp;
-
-                return value;
+            Node<T>* temp = head;
+            Node<T>* prev = nullptr;
+            
+            while(temp->get_next() != nullptr) {
+                prev = temp;
+                temp = temp->get_next();
             }
+
+            T value = temp->get_value();
+
+            if(prev != nullptr)
+                prev->set_next(nullptr);
+
+            delete temp;
+
+            return value;
         }
 
         bool extract_element(T value) {
-
-            if(this->is_Empty()) {
-                cout << "\nEmpty list!\n";
+            
+            if(this->is_empty())
                 exit(EXIT_FAILURE);
-            } else {
 
-                if(value == head->get_value()) {
-                    extract_head();
-                    return true;
-                }
-
-                Node<T>* temp = head;
-                Node<T>* prev = nullptr;
-
-                while(temp != nullptr && value != temp->get_value()) {
-                    prev = temp;
-                    temp = temp->get_next();
-                }
-
-                if(temp == nullptr)
-                    return false;
-
-                if(prev == nullptr)
-                    head = head->get_next();
-                else    
-                    prev->set_next(temp->get_next());
-                    
-                delete temp;
-
+            if(value == head->get_value()) {
+                extract_head();
                 return true;
             }
+            
+            Node<T>* temp = head;
+            Node<T>* prev = nullptr;
+
+            while(temp != nullptr && value != temp->get_value()) {
+                prev = temp;
+                temp = temp->get_next();
+            }
+
+            if(temp == nullptr) // not found
+                return false;
+
+            // found
+            if(prev != nullptr)
+                prev->set_next(temp->get_next());
+            else
+                head = head->get_next();
+
+            delete temp;
+
+            return true;
         }
 
         void print_list() const {
-            
-            Node<T>* temp = head;
 
-            while(temp != nullptr) {
-                cout << temp->get_value() << " -> ";
-                temp = temp->get_next();
+            Node<T>* current = head;
+
+            while(current != nullptr) {
+                cout << current->get_value() << " -> ";
+                current = current->get_next();
             }
 
             cout << "nullptr\n";
@@ -184,34 +183,30 @@ class List {
 };
 
 int main() {
-    
+
     List<int> list;
-    /*
-    list.push_head(69);
-    list.push_tail(420);
-    list.push_head(90);
-    list.push_tail(45);
+
+    list.push_head(5);
+    list.push_head(7);
+    list.push_head(1);
+    list.push_head(4);
+    list.push_head(9);
 
     list.print_list();
 
-    int value1 = list.extract_head();
+    list.extract_head();
     list.print_list();
 
-    int value2 = list.extract_tail();
+    list.extract_tail();
     list.print_list();
 
-    list.push_tail(42);
-    list.push_tail(100);
+    list.extract_element(1);
     list.print_list();
 
-    int value3 = list.extract_element(42);
-    */
-    list.push_sorted(50);
-    list.push_sorted(20);
-    list.push_sorted(60);
-    list.push_sorted(30);
-    list.push_sorted(40);
-    
+    list.push_sorted(5);
+    list.push_sorted(1);
+    list.push_sorted(9);
+
     list.print_list();
 
     return 0;
