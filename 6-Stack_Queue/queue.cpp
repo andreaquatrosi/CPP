20d7569426c0b4f4da8f26df688054d3b81847df
Quacciosi <andreaquatrosi@gmail.com>
2024-09-08 12:34:21 +0200

    commit from laptop

--- 6-Stack_Queue/queue.cpp
+++ 6-Stack_Queue/queue.cpp
@@ -25,61 +25,56 @@ template <typename T>
 class List {
     private:
         Node<T>* head;
+        Node<T>* tail;
 
     public:
-        List() : head(nullptr) {}
+        List() : head(nullptr), tail(nullptr) {}
 
         ~List() {
-
-            while(!is_empty())
+            while (!is_empty())
                 dequeue();
         }
 
+        // Operazioni
         bool is_empty() { return head == nullptr; }
 
         void enqueue(const T value) {
-
             Node<T>* newNode = new Node<T>(value);
 
-            if(is_empty()) {
-                head = newNode;
-
-                return;
+            if (is_empty()) {
+                head = tail = newNode;
+            } else {
+                tail->set_next(newNode); // Link the current tail to the new node
+                tail = newNode;           // Update tail to the new node
             }
-
-            Node<T>* current = head;
-
-            while(current->get_next() != nullptr)
-                current = current->get_next();
-
-            current->set_next(newNode);
         }
 
         T dequeue() {
-
-            if(is_empty())
-                exit(EXIT_FAILURE);
+            if (is_empty()) {
+                cout << "Queue is empty!" << endl;
+                exit(EXIT_FAILURE); // Or throw an exception
+            }
 
             Node<T>* temp = head;
-
             T value = head->get_value();
             head = head->get_next();
+
+            if (head == nullptr)
+                tail = nullptr;
 
             delete temp;
 
             return value;
         }
 
-        T peek() {
-
+        T peek() const {
             return head->get_value();
         }
 
         void display() const {
-            
             Node<T>* current = head;
 
-            while(current != nullptr) {
+            while (current != nullptr) {
                 cout << current->get_value() << " -> ";
                 current = current->get_next();
             }
@@ -102,7 +97,7 @@ class Queue {
             list.dequeue();
         }
 
-        T peek() {
+        T peek() const {
             return list.peek();
         }
 
@@ -112,13 +107,12 @@ class Queue {
 };
 
 int main() {
-
     Queue<int> queue;
 
     int risposta;
 
     do {
-        cout << "Scegli un opzione [Inserire -1 per uscire]:";
+        cout << "Scegli un'opzione [Inserire -1 per uscire]:";
         cout << "\n[1] Enqueue: "
              << "\n[2] Dequeue: "
              << "\n[3] Peek: "
@@ -128,27 +122,26 @@ int main() {
 
         system("CLS");
 
-        switch(risposta) {
-            case 1:
-                int value;
-                cout << "\nInserisci un valore: ";
-                cin >> value;
-
-                queue.enqueue(value);
-                break;
-            case 2:
-                queue.dequeue();
-                break;
-            case 3:
-                cout << "\nLa coda e': " << queue.peek() << "\n";
-                break;
-            case 4:
-                queue.display();
-                break;
-            default:
-                cout << "\nNow exiting...\n";
+        switch (risposta) {
+        case 1:
+            int value;
+            cout << "\nInserisci un valore: ";
+            cin >> value;
+            queue.enqueue(value);
+            break;
+        case 2:
+            queue.dequeue();
+            break;
+        case 3:
+            cout << "\nLa coda e': " << queue.peek() << "\n";
+            break;
+        case 4:
+            queue.display();
+            break;
+        default:
+            cout << "\nNow exiting...\n";
         }
-    } while(risposta != -1);
-    
+    } while (risposta != -1);
+
     return 0;
 }
